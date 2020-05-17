bbe03cc80d4b5bc0a91d8b3e264c14a3535e5b71
karrarsaad <kararmax124@gmail.com>
2020-05-17 18:42:50 +0300

    first commit

---
+++ app/Http/Controllers/Fronts/FirsstCOntroller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Fronts;
+
+use Illuminate\Http\Request;
+
+class FirsstCOntroller extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/SecondtCOntroller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SecondtCOntroller extends Controller
+{
+    public  function  show(){
+        return'second ';
+    }
+    public  function  show2(){
+        return'second ';
+    }
+}
